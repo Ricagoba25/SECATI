b15d7b132c2ab37b26ab17c23a421b7ee2ad37a7
Ricardo_Gonzalez_Bahena <20173ti128@utez.edu.mx>
2023-11-09 19:46:16 -0600

    Actualizacion

---
+++ src/main/webapp/js/metodos.js
@@ -0,0 +1,497 @@
+//*************************************************************** Funcionabilidad ***************************************************************
+function rojo1(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarfun"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f30606";
+    result1.textContent = 0;
+    sumatoria();
+
+
+}
+
+function naranja1(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarfun"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f86106";
+    result1.textContent = 25;
+    sumatoria();
+}
+
+function amarillo1(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarfun"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f5de03";
+    result1.textContent = 50;
+    sumatoria();
+}
+
+function verde1(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarfun"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#3bee07";
+    result1.textContent = 75;
+    sumatoria();
+}
+function azul1(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarfun"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#049eef";
+    result1.textContent = 100;
+    sumatoria();
+}
+
+//*************************************************************** Confiabilidad ***************************************************************
+function rojo2(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcon"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f30606";
+    result2.textContent = 0;
+    sumatoria();
+}
+
+function naranja2(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcon"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f86106";
+    result2.textContent = 25;
+    sumatoria();
+}
+
+function amarillo2(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcon"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f5de03";
+    result2.textContent = 50;
+    sumatoria();
+}
+
+function verde2(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcon"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#3bee07";
+    result2.textContent = 75;
+    sumatoria();
+}
+function azul2(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcon"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#049eef";
+    result2.textContent = 100;
+    sumatoria();
+}
+//*************************************************************** Usabilidad ***************************************************************
+function rojo3(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarusa"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f30606";
+    result3.textContent = 0;
+    sumatoria();
+}
+
+function naranja3(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarusa"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f86106";
+    result3.textContent = 25;
+    sumatoria();
+}
+
+function amarillo3(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarusa"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f5de03";
+    result3.textContent = 50;
+    sumatoria();
+}
+
+function verde3(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarusa"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#3bee07";
+    result3.textContent = 75;
+    sumatoria();
+}
+function azul3(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarusa"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#049eef";
+    result3.textContent = 100;
+    sumatoria();
+}
+//*************************************************************** Rendimiento ***************************************************************
+function rojo4(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarren"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f30606";
+    result4.textContent = 0;
+    sumatoria();
+}
+
+function naranja4(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarren"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f86106";
+    result4.textContent = 25;
+    sumatoria();
+}
+
+function amarillo4(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarren"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f5de03";
+    result4.textContent = 50;
+    sumatoria();
+}
+
+function verde4(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarren"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#3bee07";
+    result4.textContent = 75;
+    sumatoria();
+}
+function azul4(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarren"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#049eef";
+    result4.textContent = 100;
+    sumatoria();
+}
+//*************************************************************** Mantenimiento ***************************************************************
+function rojo5(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarman"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f30606";
+    result5.textContent = 0;
+    sumatoria();
+}
+
+function naranja5(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarman"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f86106";
+    result5.textContent = 25;
+    sumatoria();
+}
+
+function amarillo5(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarman"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f5de03";
+    result5.textContent = 50;
+    sumatoria();
+}
+
+function verde5(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarman"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#3bee07";
+    result5.textContent = 75;
+    sumatoria();
+}
+function azul5(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarman"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#049eef";
+    result5.textContent = 100;
+    sumatoria();
+}
+//*************************************************************** Portabilidad ***************************************************************
+function rojo6(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarpor"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f30606";
+    result6.textContent = 0;
+    sumatoria();
+}
+
+function naranja6(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarpor"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f86106";
+    result6.textContent = 25;
+    sumatoria();
+}
+
+function amarillo6(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarpor"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f5de03";
+    result6.textContent = 50;
+    sumatoria();
+}
+
+function verde6(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarpor"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#3bee07";
+    result6.textContent = 75;
+    sumatoria();
+}
+function azul6(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarpor"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#049eef";
+    result6.textContent = 100;
+    sumatoria();
+}
+//*************************************************************** Seguridad ***************************************************************
+function rojo7(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarseg"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f30606";
+    result7.textContent = 0;
+    sumatoria();
+}
+
+function naranja7(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarseg"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f86106";
+    result7.textContent = 25;
+    sumatoria();
+}
+
+function amarillo7(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarseg"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f5de03";
+    result7.textContent = 50;
+    sumatoria();
+}
+
+function verde7(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarseg"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#3bee07";
+    result7.textContent = 75;
+    sumatoria();
+}
+function azul7(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarseg"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#049eef";
+    result7.textContent = 100;
+    sumatoria();
+}
+//*************************************************************** Compatibilidad ***************************************************************
+function rojo8(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcom"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f30606";
+    result8.textContent = 0;
+    sumatoria();
+}
+
+function naranja8(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcom"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f86106";
+    result8.textContent = 25;
+    sumatoria();
+}
+
+function amarillo8(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcom"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#f5de03";
+    result8.textContent = 50;
+    sumatoria();
+}
+
+function verde8(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcom"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#3bee07";
+    result8.textContent = 75;
+    sumatoria();
+}
+function azul8(tdID) {
+    var tds = document.querySelectorAll('td[id^="pintarcom"]');
+    tds.forEach(function(td) {
+        td.style.backgroundColor = "white";
+    });
+
+    var td = document.getElementById(tdID);
+    td.style.backgroundColor = "#049eef";
+    result8.textContent = 100;
+    sumatoria();
+}
+
+
+//*************************************************************** Asignacion de valores ***************************************************************
+
+
+function sumatoria() {
+    var val1 = Number(document.getElementById("result1").textContent);
+    var val2 = Number(document.getElementById("result2").textContent);
+    var val3 = Number(document.getElementById("result3").textContent);
+    var val4 = Number(document.getElementById("result4").textContent);
+    var val5 = Number(document.getElementById("result5").textContent);
+    var val6 = Number(document.getElementById("result6").textContent);
+    var val7 = Number(document.getElementById("result7").textContent);
+    var val8 = Number(document.getElementById("result8").textContent);
+
+    var sumaTo = (val1 + val2 + val3 + val4 + val5 + val6 + val7 + val8) / 8;
+
+
+    totalTotal.textContent = sumaTo.toFixed(2);
+}
+
+
